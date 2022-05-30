dbd234e4a7e44087863b3d4b36c77aca870f66ce
balti4152 <56260411+balti4152@users.noreply.github.com>
2022-05-30 17:20:40 -0300

    Add files via upload

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return (
+        <nav>
+            
+            <button>OPCION 1</button>
+            <button>OPCION 2</button>
+            <button>OPCION 3</button>
+        </nav>
+    )
+}
+
+export default NavBar
